f0dc5744b2265bc6cf647877d495eb0d0fcc86a1
Lucas Polesello <lucas.c4d@gmail.com>
2022-09-13 19:36:38 -0300

    finished 9.3-warehouse-item-state

--- 9-behavioural-design-patterns/9.3-warehouse-item.js
+++ 9-behavioural-design-patterns/9.3-warehouse-item.js
@@ -15,3 +15,120 @@ It cannot be delivered if never stored.
 
 STATE PATTERN
  */
+
+class WarehouseItemStoreState {
+
+    constructor(warehouse, locationId) {
+        warehouse.locationId = locationId;
+        this._warehouse = warehouse;
+        Object.defineProperty(this, '_canChange', {
+            value: function () {
+                return !(this._warehouse._state instanceof WarehouseItemDeliveredState);
+            }
+        })
+
+        Object.defineProperty(this, '_canSet', {
+            value: function () {
+                return this._warehouse._state instanceof WarehouseItemArrivingState;
+            }
+        })
+    }
+
+    describe() {
+        return `Item ${this._warehouse._id} its on location ${this._warehouse.locationId}`;
+    }
+
+}
+
+class WarehouseItemDeliveredState {
+
+    constructor(warehouse, address) {
+        warehouse._address = address;
+        this._warehouse = warehouse;
+        Object.defineProperty(this, '_canChange', {
+            value: function () {
+                return this._warehouse._state instanceof WarehouseItemStoreState;
+            }
+        })
+
+        Object.defineProperty(this, '_canSet', {
+            value: function () {
+                return this._warehouse._state && this._warehouse._state instanceof WarehouseItemStoreState;
+            }
+        })
+    }
+
+    describe() {
+        return `Item ${this._warehouse._id} is on ${this._warehouse._address}`;
+    }
+
+}
+
+class WarehouseItemArrivingState {
+
+    constructor(warehouse) {
+        this._warehouse = warehouse;
+        Object.defineProperty(this, '_canChange', {
+            value: function () {
+                return ![WarehouseItemStoreState, WarehouseItemDeliveredState].some((c) => this._warehouse._state instanceof c);
+            }
+        })
+
+        Object.defineProperty(this, '_canSet', {
+            value: function () {
+                return !this._warehouse._state;
+            }
+        })
+    }
+
+    describe() {
+        return `Item ${this._warehouse._id} is arriving`;
+    }
+
+}
+
+class WarehouseItem {
+
+    constructor(id, state) {
+        this._id = id;
+
+        Object.defineProperty(this, '_changeState', {
+            value: function (state) {
+                if (!this._state) {
+                    this._state = new WarehouseItemArrivingState(this);
+                    return;
+                }
+
+                if (this._state._canChange() && state._canSet && state._canSet()) {
+                    this._state = state;
+                } else {
+                    throw new Error(`Cannot change ${this._state.constructor.name} to ${state.constructor.name}`)
+                }
+            }
+        })
+
+        this._changeState(state);
+    }
+
+
+    store(locationId) {
+        this._changeState(new WarehouseItemStoreState(this, locationId));
+    }
+
+    deliver(address) {
+        this._changeState(new WarehouseItemDeliveredState(this, address))
+    }
+
+    describe() {
+        return this._state.describe();
+    }
+}
+
+const item = new WarehouseItem(10);
+console.log(item.describe());
+
+item.store(1231231);
+console.log(item.describe());
+
+item.deliver(`Rua dos Guri`);
+console.log(item.describe());
