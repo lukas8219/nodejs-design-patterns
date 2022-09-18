17e60441b25607ee37b3d95cc5aeb6376fa7e6a4
Lucas Polesello <lucas.c4d@gmail.com>
2022-09-17 21:30:03 -0300

    finished 9.4 - logging with middlewares

--- 9-behavioural-design-patterns/9.4-logging-with-middleware.js
+++ 9-behavioural-design-patterns/9.4-logging-with-middleware.js
@@ -7,3 +7,53 @@ the message and how to output them
 We could for example add a serialize() middleware to convert log messages to string representation ready to be sent over wire or saved somewhere.
 Then we could add, saveToFile() middlewares that saves each message to a file.
  */
+import { createWriteStream } from 'fs';
+import { Socket, Server } from 'net';
+import { Readable } from 'stream';
+
+const server = new Server().listen(8080, () => console.log('WORKED!'))
+
+server.on('connection', (stream) => {
+    stream.pipe(createWriteStream('my-log-tcp.log', 'utf-8'))
+})
+
+const socket = new Socket()
+    .connect({
+        localAddress: '127.0.0.1',
+        port: 8080,
+    });
+
+function createLogWithMiddleware(middleware){
+    return function(...messages){
+        const exec = function(middleware){
+            middleware && typeof middleware === 'function' &&  middleware(...messages);    
+        } 
+        console.log(...messages);
+        if(Array.isArray(middleware)){
+            middleware.forEach(exec);
+            return;
+        }
+        exec(middleware)
+    }
+}
+
+
+function saveToFile(){
+    return function(...messages){
+        Readable.from(messages)
+        .pipe(createWriteStream('my-logs.txt', 'utf-8'))
+    }
+}
+
+function serialize(){
+   return  function(...message){
+        Readable.from(message)
+        .pipe(socket)
+    }
+}
+
+const logger = createLogWithMiddleware([saveToFile(), serialize()])
+logger('THIS IS MY MESSAGE!')
+
+//TODO evolve this! Add error handling! Improve performance of streams
+//Add next method calling
