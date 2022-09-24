de5a1e1ef2e0d4107af597dfdb05f0d56c995d00
Lucas Polesello <lucas.c4d@gmail.com>
2022-09-24 17:50:43 -0300

    feat: finis 11.1 proxy with initialization queue

--- 11-advanced-recipes/11.1-proxy-with-pre-initialization-queues.js
+++ 11-advanced-recipes/11.1-proxy-with-pre-initialization-queues.js
@@ -1,5 +1,46 @@
 /* 
-using java script proxy object, create a wrapper for adding pre initialization queues to any object.
-
-You should allow the consumer of the wrapper to decide which methods to augment and the name of the property/event that indicates if the component is initialized or not.
+Using a JavaScript Proxy, create a wrapper for adding pre-initialization queues to any object.
+You should allow the consumer of the wrapper to decide which methods to augment and the name of the property/event that indicates if the component is initialized.
 */
+function createAsyncProxy(target, proxyProperties, initializer) {
+  const isProxyProperty = (property) => {
+    if (Array.isArray(proxyProperties)) {
+      return proxyProperties.includes(property);
+    }
+    return property === proxyProperties;
+  };
+  const queue = [];
+
+  return new Proxy(target, {
+    get(t, property) {
+      if (property === initializer) {
+        return t[property];
+      }
+      if (t[initializer]) {
+        return t[property];
+      }
+      if (isProxyProperty(property)) {
+        return new Promise((res, rej) => {
+          queue.push(() => res(t[property]));
+        });
+      }
+      return t[property];
+    },
+    set(t, property, value) {
+      if (property === initializer) {
+        process.nextTick(() => queue.forEach((task) => task()));
+      }
+      return (t[property] = value);
+    },
+  });
+}
+
+const obj = { name: "Lucas" };
+
+const wr = createAsyncProxy(obj, "name", "age");
+
+wr.name.then((name) => console.log(name));
+
+setTimeout(() => {
+  wr.age = 50;
+}, 500);
