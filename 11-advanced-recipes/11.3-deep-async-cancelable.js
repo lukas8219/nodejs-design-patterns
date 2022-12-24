355ec3e77ef132d0421ee09a8f942f5e0e3f20b2
Lucas Polesello <lucas.c4d@gmail.com>
2022-12-24 16:54:43 -0300

    feat: finished 11.3 and 11.4 - deep async cancellable and compute farm

--- 11-advanced-recipes/11.3-deep-async-cancelable.js
+++ 11-advanced-recipes/11.3-deep-async-cancelable.js
@@ -1,5 +1,94 @@
 /* 
-Extend the createAsynCancelable function so that its possible to invoke other cancelable functions from within the main cacheable function.
+Extend the createAsynCancelable function so that its possible to invoke other cancelable functions from within the main cancelable function.
 Cancelling the main operation should also cancel all nested operations
 hint: Allow to yield the result of an asyncCancelable from within the generator function.
  */
+
+function createAsyncCancelable(generationFunction) {
+    return function asyncCancelable(...args) {
+        const generator = generationFunction(...args);
+
+        let canceled = false;
+        const toBeCancelled = [];
+
+        function cancel() {
+            toBeCancelled.forEach((fn) => fn());
+            canceled = true;
+        }
+
+        const promise = new Promise((res, rej) => {
+
+            async function next(prevResult) {
+
+                if (prevResult.value && typeof prevResult.value.cancel === 'function') {
+                    toBeCancelled.push(prevResult.value.cancel)
+                }
+
+                if (canceled) {
+                    return rej(new Error('canceled!'));
+                }
+
+                if (prevResult.done) {
+                    return res(prevResult.value);
+                }
+                try {
+                    await next(await generator.next(await (await prevResult.value)))
+                } catch (err) {
+                    try {
+                        await next(await generator.throw(err));
+                    } catch (err2) {
+                        return rej(err2);
+                    }
+                }
+
+            }
+
+            next({});
+        })
+
+        return { promise, cancel }
+    }();
+}
+
+//TEST
+
+function asyncFn(fn, ms = 1000) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            try {
+                return res(fn());
+            } catch (err) {
+                return rej(err);
+            }
+        }, ms)
+    })
+}
+
+function createFlow(name) {
+    return createAsyncCancelable(function* () {
+        try {
+            yield console.log(`starting flow ${name}`);
+            const a = yield asyncFn(() => 100);
+            const b = yield asyncFn(() => 200);
+            yield asyncFn(() => console.log(a + b));
+            yield console.log(`finishing flow ${name}`);
+        } catch (err) {
+            console.log(err);
+        }
+    });
+}
+
+const { promise, cancel } = createAsyncCancelable(function* () {
+    try {
+        yield createFlow('1');
+        yield createFlow('2');
+        yield createFlow('3');
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+setTimeout(() => {
+    cancel()
+})
--- 11-advanced-recipes/11.4-compute-farm.js
+++ 11-advanced-recipes/11.4-compute-farm.js
@@ -3,3 +3,61 @@ Create a HTTP server with a POST endpoint that receives as input the code of a f
 and returns the result back to the client.
 hint you can use evail() vm.runInContext or neither of the two.
  */
+import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+import { compileFunction, createContext, Script } from 'vm';
+import { parentPort, isMainThread, workerData, Worker } from 'worker_threads'
+
+if(isMainThread){
+    createServer(async (req, res) => {
+        const ENDPOINT = /run-in-vm/
+        if (req.method === 'POST' && req.url.match(ENDPOINT)) {
+                const buffer = [];
+    
+                for await (const chunk of req) {
+                    buffer.push(chunk);
+                }
+    
+                const { fn, args } = JSON.parse(Buffer.concat(buffer));
+    
+                const worker = new Worker(fileURLToPath(import.meta.url), {
+                    workerData: { fn, args }
+                });
+    
+                ["exit", "error"].forEach((event) => {
+                    worker.on(event, (error) => {
+                        if(res.headersSent){
+                            return;
+                        }
+                        if(error !== 0){
+                            res.writeHead(500);
+                            res.write(JSON.stringify({ message: error, code: 500 }));
+                            console.log(error);
+                            return res.end();
+                        }
+                    });
+                })
+
+                worker.on('message', (message) => {
+                        res.writeHead(200, {
+                         'Content-Type': 'application/json'
+                        });
+                        res.write(JSON.stringify({ result: message, fn, args }));
+                        return res.end();
+                })
+        }
+    }).listen(8080);
+} else {
+    const { fn, args } = workerData;
+
+    const script = new Script(`
+    var fn = ${fn};
+    `);
+
+    const context = createContext({});
+    script.runInNewContext(context);
+
+    const result = context['fn'](...args)
+
+    parentPort.postMessage(result);
+}
